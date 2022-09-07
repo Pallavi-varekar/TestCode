1bcb4a4ad60f877f9ccc38aa80f4d0ed47ec2020
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-09-07 16:30:36 +0530

    Initial commit

---
+++ src/TestCode.java
@@ -0,0 +1,11 @@
+
+public class TestCode {
+
+    public static void main(String[] args) {
+    }
+    static void printMessage( ){
+            System.out.println("Welcome To Employee Wage");
+
+        }
+    }
+
