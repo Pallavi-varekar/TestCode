892793703e18c5ac74bc27bbb331c4d1208fe2bc
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-09-07 16:37:14 +0530

    added:welcome message

--- src/TestCode.java
+++ src/TestCode.java
@@ -3,7 +3,7 @@ public class TestCode {
 
     public static void main(String[] args) {
     }
-    static void printMessage( ){
+    static {
             System.out.println("Welcome To Employee Wage");
 
         }
